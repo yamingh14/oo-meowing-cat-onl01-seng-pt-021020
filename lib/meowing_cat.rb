9a4728e570567b0950faf83fcc64e2138c796d7b
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-12 02:17:54 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,7 +1,15 @@
 class Cat
-  attr_accessor :meow, :name
+  attr_accessor :meow
   def initialize(sound, snoopy)
     @meow = sound
     @name = snoopy
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
   end
 end
